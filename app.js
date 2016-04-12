231385407629f97521981c10070e9e22ba3c1bfa
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-04-12 10:31:47 +0200

    newaws.js implemented (draft #1)

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
 
 var AWS = require('aws-sdk');
-AWS.config.loadFromPath('./config.json');
+AWS.config.loadFromPath('/config.json');
 var awsinfo = require("./awsinfo").lab;
+var newaws = require("./newaws").lab;
 
 var PORT = 8080;
 
@@ -11,10 +12,14 @@ var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
-	{path: "/awsinfo", action: awsinfo}, 
+	{path: "/newaws", action: newaws}, 
 	];
 
 var service = require("./lib/service").http(urlMap);
+
+function(request, callback) {
+
+}
 
 service(PORT);
 
