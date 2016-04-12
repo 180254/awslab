775b412350023f17f14452eea0e08304dabd49aa
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-04-12 09:03:12 +0000

    newaws: getting public ip draft

--- newaws.js
+++ newaws.js
@@ -1,4 +1,5 @@
 var AWS = require('aws-sdk');
+var ec2 = new AWS.EC2();
 
 var task =  function(request, callback){
 
@@ -7,14 +8,47 @@ var task =  function(request, callback){
 	  MaxCount: 1, /* required */
 	  MinCount: 1, /* required */
 	  Monitoring: {
-		Enabled: false /* required */
+		Enabled: true /* required */
 	  },
+  	NetworkInterfaces: [
+    		{
+     		 AssociatePublicIpAddress: true,
+		DeviceIndex: 0,
+		}
+	],
+
 	};
+
+
+var params2 = {
+          DryRun:  false,
+          MaxResults: 5
+        };
+
 	
 	var ec2 = new AWS.EC2();
 	ec2.runInstances(params, function(err, data) {
 	  if (err)callback(null, err.stack); // an error occurred
-	  else     callback(null,data);           // successful response
+	  else{   
+
+var rId = data.ReservationId;
+
+ec2.describeInstances(params, function(err, data) {
+          if (err) callback(null, err.stack); // an error occurred
+          else     {
+
+for (r in data.Reservations) {
+if(r.ReservationId == "rID") {
+var ip = r.Instances[0].PublicIpAddress;
+callback(null, ip);
+}          // successful response
+        });
+}
+
+
+
+
+ }  // successful response
 	});
 	
 }
