57867dadff54ce22c26717a290a64a7ca09ad2f8
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-04-12 11:06:10 +0200

    newaws.js reformated

--- newaws.js
+++ newaws.js
@@ -1,56 +1,58 @@
 var AWS = require('aws-sdk');
 var ec2 = new AWS.EC2();
 
-var task =  function(request, callback){
+var task = function(request, callback) {
 
-	var params = {
-	  ImageId: 'ami-80f715e0', /* required */
-	  MaxCount: 1, /* required */
-	  MinCount: 1, /* required */
-	  Monitoring: {
-		Enabled: true /* required */
-	  },
-  	NetworkInterfaces: [
-    		{
-     		 AssociatePublicIpAddress: true,
-		DeviceIndex: 0,
-		}
-	],
+        var params = {
+            ImageId: 'ami-80f715e0',
+            /* required */
+            MaxCount: 1,
+            /* required */
+            MinCount: 1,
+            /* required */
+            Monitoring: {
+                Enabled: true /* required */
+            },
+            NetworkInterfaces: [{
+                AssociatePublicIpAddress: true,
+                DeviceIndex: 0,
+            }],
 
-	};
-
-
-var params2 = {
-          DryRun:  false,
-          MaxResults: 5
         };
 
-	
-	var ec2 = new AWS.EC2();
-	ec2.runInstances(params, function(err, data) {
-	  if (err)callback(null, err.stack); // an error occurred
-	  else{   
 
-var rId = data.ReservationId;
-
-ec2.describeInstances(params, function(err, data) {
-          if (err) callback(null, err.stack); // an error occurred
-          else     {
-
-for (r in data.Reservations) {
-if(r.ReservationId == "rID") {
-var ip = r.Instances[0].PublicIpAddress;
-callback(null, ip);
-}          // successful response
-        });
-}
+        var params2 = {
+            DryRun: false,
+            MaxResults: 5
+        };
 
 
+        var ec2 = new AWS.EC2();
+        ec2.runInstances(params, function(err, data) {
+                if (err) callback(null, err.stack); // an error occurred
+                else {
+
+                    var rId = data.ReservationId;
+
+                    ec2.describeInstances(params, function(err, data) {
+                                if (err) callback(null, err.stack); // an error occurred
+                                else {
+
+                                    for (r in data.Reservations) {
+                                        if (r.ReservationId == "rID") {
+                                            var ip = r.Instances[0].PublicIpAddress;
+                                            callback(null, ip);
+                                        } // successful response
+                                    };
+                            }
 
 
- }  // successful response
-	});
-	
-}
 
-exports.lab = task
+
+                        }); // successful response
+                };
+
+        }
+	}
+
+        exports.lab = task
