eda28e2cdb39143fbd17cd50d0281c2f52fe388b
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-04-12 08:39:58 +0000

    awsnew: instance is creating

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
 
 var AWS = require('aws-sdk');
-AWS.config.loadFromPath('/config.json');
+AWS.config.loadFromPath('./config.json');
 var awsinfo = require("./awsinfo").lab;
 var newaws = require("./newaws").lab;
 
@@ -12,14 +12,11 @@ var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
+	{path: "/awsinfo", action: awsinfo},
 	{path: "/newaws", action: newaws}, 
 	];
 
 var service = require("./lib/service").http(urlMap);
-
-function(request, callback) {
-
-}
 
 service(PORT);
 
