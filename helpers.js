a5aef68a85c969183b2b4a94971525cbd727e54b
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-04-13 18:42:56 +0200

    task completed and refactored

--- helpers.js
+++ helpers.js
@@ -11,5 +11,4 @@ var hash = function(algorithm, text){
 	}
 	return digest;
 }
-
 exports.hash = hash;
--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,11 +1,54 @@
-//stub for lab 1_2
 var AWS = require('aws-sdk');
-
 AWS.config.loadFromPath('./config.json');
+var ec2 = new AWS.EC2();
 
-var task =  function(request, callback){
-	
-	
-}
+var aws_info = function (request, callback) {
+    var params = {
+        DryRun: false,
+        MaxResults: 99
+    };
 
-exports.lab = task
+    ec2.describeInstances(params, function (err, data) {
+        if (err) callback(null, err.stack);
+        else     callback(null, data);
+    });
+};
+
+var aws_new = function (request, callback) {
+    var params = {
+        ImageId: 'ami-80f715e0',
+        MaxCount: 1,
+        MinCount: 1,
+        Monitoring: {
+            Enabled: false
+        },
+        InstanceType: 't1.micro',
+        KeyName: 'SuperKlucz'
+    };
+
+    var params2 = {
+        DryRun: false
+    };
+
+    ec2.runInstances(params, function (err, data) {
+        if (err) callback(null, err.stack);
+
+        else {
+            params2.InstanceIds = [data.Instances[0].InstanceId];
+            ec2.describeInstances(params2, function (err, data) {
+                if (err) callback(null, err.stack);
+
+                else {
+                    var result = {
+                        PublicIpAddress: data.Reservations[0].Instances[0].PublicIpAddress,
+                        PublicDnsName: data.Reservations[0].Instances[0].PublicDnsName
+                    };
+                    callback(null, result);
+                }
+            });
+        }
+    });
+};
+
+exports.aws_info = aws_info;
+exports.aws_new = aws_new;
